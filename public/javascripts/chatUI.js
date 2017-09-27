2bf08c3eb404195a444397bf048e585ae4fbf330
tonycweng <tcweng91@gmail.com>
2017-09-26 19:27:58 -0700

    start chatUI

--- public/javascripts/chatUI.js
+++ public/javascripts/chatUI.js
@@ -3,6 +3,18 @@ const Chat = require('./chat');
 class ChatUI {
   constructor(socket) {
     this.chat = new Chat(socket);
+    this.msgList = document.querySelector('ul#msg-list');
+    this.input = document.querySelector('input');
+  }
+
+  getInput() {
+    return this.input.value;
+  }
+
+  addMSG(msg) {
+    const newMessage = document.createElement('li');
+    newMessage.textContent = msg;
+    this.msgList.appendChild(newMessage);
   }
 }
 
